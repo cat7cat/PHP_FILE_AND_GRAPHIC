3e63327a9a3f097165da393ca88a056f531328f9
chrislin <gi.chrislin@gmail.com>
2022-10-07 15:43:21 +0800

    新增array_pra07.php 旋轉陣列

--- php/basic/array/array_pra07 .php
+++ php/basic/array/array_pra07 .php
@@ -8,7 +8,24 @@
 </head>
 <body>
 
+<h1>旋轉陣列</h1>
+<h3>給定一個陣列有K個元素及旋轉N的次數,回傳旋轉完成後的陣列</h3>
+<?php
 
+$array=[3,2,1,5,6];
+$n=3;
+$t=0;
+$k=count($array);
+for($i=0;$i<$n;$i++){
+    $t=$array[$k-1];
+    for($j=0;$j<$K;$j++){
+        $array[$k-1]=$array[$k-2];
+    }
+} echo $t;
+
+
+
+?>
     
 </body>
 </html>
